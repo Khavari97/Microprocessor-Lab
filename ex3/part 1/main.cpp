caab6902d27b7dea3b1dab39bb7b35c64d36b161
Zahra Khavari <Khavari.khu@gmail.com>
2021-10-16 07:17:13 +0330

    some change

--- ex3/part 1/main.cpp
+++ ex3/part 1/main.cpp
@@ -1,23 +1,65 @@
 #include <avr/io.h>
 #include <avr/delay.h>
+#include <util/atomic.h>
 
-int i = 0;
-// for anod 0b11000000,0b11111001,0b10100100,0b10110000,0b10011001,0b10010010,0b100000010,0b11111000,0b10000000,0b10010000
-// 0-9 binary (catod)
-int numbers[10] = { 0b00111111,0b00000110,0b01011011,0b01001111,0b01100110,0b01101101,0b011111101,0b00000111,0b01111111,0b01101111};
-                  
-int main() {
+int main() 
+{
+    TIMSK = 0b00000100;
+    TCCR1B = 0b0000010;
+    DDRD = 0xff;
+    DDRC = 0xff;
+    sei();
+    PORTA = 0;
+    PORTD = 0;
 
-        PA0 == 0; 
-        DDRD = 0xFF;
-
-        if (PINA ==1)
-        {
-            for(i = 0 ; i < 10 ; i++){
-                PORTD = numbers[i];
-                _delay_ms(50);
-            }
-            
-        }
-
+    while (true)
+    {
+        
+    }
+    
+    return 0;
 }
+//  0b11000000,0b11111001,0b10100100,0b10110000,0b10011001,0b10010010,0b100000010,0b11111000,0b10000000,0b10010000
+int numbers = 0;
+ISR(TIMER1_OVF_vect){
+    PA0 ==0;
+       if(PINA == 1){
+            numbers = (numbers + 1) % 10;
+    switch(numbers){
+        case 0:
+            PORTD = 0b0111111;
+            break;
+        case 1:
+            PORTD = 0b0000110;
+            break;
+        case 2:
+            PORTD = 0b1011011;
+            break;
+        case 3:
+            PORTD = 0b1001111;
+            break;
+        case 4:
+            PORTD = 0b1100110;
+            break;
+        case 5:
+            PORTD = 0b1101101;
+            break;
+        case 6:
+            PORTD = 0b1111101;
+            break;
+        case 7:
+            PORTD = 0b0000111;
+            break;
+        case 8:
+            PORTD = 0b1111111;
+            break;
+        case 9:
+            PORTD = 0b1101111;
+            break;
+        default:
+            PORTD = 0xFF;
+    }
+       }
+}
+
+
