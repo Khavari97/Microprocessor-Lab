80e8bad428111fef825d602ebbd535cabdae08b4
Zahra Khavari <Khavari.khu@gmail.com>
2021-10-22 15:41:34 +0330

    create part 1 of ex4

---
+++ ex4/part 1/main.cpp
@@ -0,0 +1,29 @@
+#include <avr/io.h>
+#include <avr/mega16a.h>
+#include <avr/alcd.h>
+#include <avr/delay.h>
+
+
+
+int main() {
+   
+    DDRC=(1<<DDC7) | (1<<DDC6) | (1<<DDC5) | (1<<DDC4) | (1<<DDC3) | (1<<DDC2) | (1<<DDC1) | (1<<DDC0);
+    PORTC=(0<<PORTC7) | (0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+    lcd_init(16);
+
+while (1)
+      {
+       lcd_clear();
+       lcd_gotoxy(0,0);
+       lcd_putsf('Zahra Khavari');
+       _delay_ms(500);
+       
+      }
+    
+    
+    
+
+
+
+   
+}
