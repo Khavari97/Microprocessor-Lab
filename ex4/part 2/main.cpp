c40665222b2fb716e473b3ddb181e330204ee7be
Zahra Khavari <Khavari.khu@gmail.com>
2021-10-22 15:51:21 +0330

    added part 2 files

---
+++ ex4/part 2/main.cpp
@@ -0,0 +1,18 @@
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <avr/delay.h>
+
+
+int main() {
+
+    DDRA = 0xFF;
+    PORTA = 0;
+    while (1)
+    {
+        PORTA = 0xFF;
+        _delay_ms(500);
+        PORTA = 0;
+         _delay_ms(500);
+    }
+    return 0;
+}
