3c29f6a31cbc207cc717d5dc8a51bcea5534df1d
Zahra Khavari <Khavari.khu@gmail.com>
2021-10-22 22:57:39 +0330

    save changes

--- ex4/part 2/main.cpp
+++ ex4/part 2/main.cpp
@@ -1,18 +1,49 @@
-#define F_CPU 1000000UL
 #include <avr/io.h>
-#include <avr/delay.h>
-
-
-int main() {
-
-    DDRA = 0xFF;
-    PORTA = 0;
-    while (1)
-    {
-        PORTA = 0xFF;
-        _delay_ms(500);
-        PORTA = 0;
-         _delay_ms(500);
+#include <stdio.h>
+#include <avr/LCD.h>
+ 
+unsigned char Buffer[17];
+unsigned char second = 30;
+unsigned char minute = 20;
+unsigned char hour = 10;
+ 
+    void time_write(){
+    sprintf(Buffer,"%02d:%02d:%02d",hour,minute,second);
+    lcd_gotoxy(4,0);
+    lcd_string_signed(Buffer);
+  
+    
     }
-    return 0;
+ 
+int main() {
+///////////////////block 1
+lcd_init();
+lcd_clear();
+///////////////////block 2
+TIMSK = 0x40;
+TIFR = 0x40;
+sei();
+///////////////////block 3
+ASSR = 0x08;
+TCNT2 = 0x00;
+TCCR2 = 0x05;
+///////////////////block 4
+time_write();
+lcd_gotoxy(3,1);
+lcd_string_signed("Start Time");
+while(1);
 }
+ 
+    ISR(TIMER2_COMP_vect){
+    second++;
+        if(second==60){
+        second = 0;
+        minute++;
+            if(minute==60){
+            minute = 0;
+            hour++;
+            if(hour==24) hour = 0;
+            }    
+        }
+    time_write();
+    }
