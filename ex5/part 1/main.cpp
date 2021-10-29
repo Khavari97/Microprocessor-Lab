76b65645ba4cf91405f7296d6f5acea5d66a6f7b
Zahra Khavari <Khavari.khu@gmail.com>
2021-10-29 21:53:45 +0330

    save changes

--- ex5/part 1/main.cpp
+++ ex5/part 1/main.cpp
@@ -7,15 +7,15 @@ int main() {
     
         DDRA=0X0F;
         PORTA=0X01;    //1
-        _delay_ms(5);
+        _delay_ms(50);
         PORTA=0X04;    //5
-        _delay_ms(5);
+        _delay_ms(50);
         PORTA=0X02;     //3
-        _delay_ms(5);
+        _delay_ms(50);
         PORTA=0X08;     //8
-        _delay_ms(5);
+        _delay_ms(50);
         PORTA=0X01;    //1
-        _delay_ms(5);
+        _delay_ms(50);
    
 
 
--- ex5/part 2/main.cpp
+++ ex5/part 2/main.cpp
@@ -6,16 +6,18 @@ int main() {
 //1 8 3 5 1
         int round[5] = {0X01,0X08,0X02,0X04,0X01};
 
-        DDRA=0X0F;
+        if((0>>PC2)==0){
+                DDRA=0X0F;
         PORTA=0X01;    //1
-        _delay_ms(5);
+        _delay_ms(50);
         PORTA=0X08;     //8
-        _delay_ms(5);
+        _delay_ms(50);
          PORTA=0X02;     //3
-        _delay_ms(5);
+        _delay_ms(50);
         PORTA=0X04;    //5
-        _delay_ms(5);
+        _delay_ms(50);
         PORTA=0X01;    //1
-        _delay_ms(5);
+        _delay_ms(50);
+        }
    
 }
