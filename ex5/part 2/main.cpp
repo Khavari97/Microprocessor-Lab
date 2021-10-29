c8c6f99108c4a633a35dce1ca16f4865bb2cbc09
Zahra Khavari <Khavari.khu@gmail.com>
2021-10-29 12:24:53 +0330

    added folder part1

---
+++ ex5/part 2/main.cpp
@@ -0,0 +1,22 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main() {
+
+    
+        DDRA=0X0F;
+        PORTA=0X01;    //1
+        _delay_ms(5);
+        PORTA=0X08;     //8
+        _delay_ms(5);
+         PORTA=0X02;     //3
+        _delay_ms(5);
+        PORTA=0X04;    //5
+        _delay_ms(5);
+        PORTA=0X01;    //1
+        _delay_ms(5);
+   
+
+
+}
