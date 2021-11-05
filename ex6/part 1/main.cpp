07f873f6947529cce156f1f39f2fab49c3e67dad
Zahra Khavari <Khavari.khu@gmail.com>
2021-11-06 02:09:35 +0330

    save changes

--- ex6/part 1/main.cpp
+++ ex6/part 1/main.cpp
@@ -1,36 +1,37 @@
-#include <avr/io.h>     
-#include <util/delay.h> 
-#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <avr/delay.h>
+#include "LCD.h"
+#include <stdio.h>
 
 
-int direction[5] = {0x01, 0x04, 0x02, 0x08,0x01};
 
 int main(){
-        
-    DDRA = 0x0f; 
-    PORTA = 0x00;
-   
-    sei();
 
-    while (true)
-    {
-        PORTC |= 0x04;
-        if (PINC == 0)
-        {
-            TCCR0 = 0x05;
-            TIMSK = 0x01;
-        }
-    }
-    return 0;
+	DDRB=0xFF;              
+	DDRA=0; 							       
+	ADCSRA = 0x87;									
+	ADMUX = 0x40;                      
+	
+	char Buffer[16];
+    int T = 0; 
+      lcd_init();    
+      //lcd_clear();
+	
+
+	while (true){
+
+            ADCSRA = (1 << ADEN) | (0 << ADSC) | (1 << ADATE) | (0 << ADIF) | (0 << ADIE) | (1 << ADPS2) | (0 << ADPS1) | (0 << ADPS0);
+	        SFIOR = (0 << ADTS2) | (0 << ADTS1) | (0 << ADTS0);
+
+            while ((ADCSRA & (1 << ADIF)) == 0); 
+	              ADCSRA |= (1 << ADIF); 
+	
+	
+		T = (int)(ADCW/2.05) ; 
+		sprintf(Buffer,"temp is = %d C",T);
+		lcd_string_signed(Buffer);
+		_delay_ms(100);
+      }
+
+	return 0;
 }
-
-int num = 0;
-ISR(TIMER0_OVF_vect){
-   
-    num++;
-    if (num > 4)
-        num = 0;
-        PORTA = direction[num];
-   
-}
-
